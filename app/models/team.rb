146e2e87fc2e6a771b8eb4b1da8a8e888571c75e
Josh Cornelius <joshcornelius9@gmail.com>
2015-01-10 17:32:29 -0500

    updated autocomplete for team settings

--- app/models/team.rb
+++ app/models/team.rb
@@ -10,17 +10,18 @@ class Team < ActiveRecord::Base
 
   def self.organization_array
   	array = []
-  	Team.all.each do |team|
-  		hash = {}
-  		hash[:value] = team.name
-      hash[:team_id] = team.id
-  		hash[:division] = team.division.name
-      hash[:division_id] = team.division.id
-  		hash[:league] = team.league.name
-      hash[:league_id] = team.league.id
-  		array.push(hash)
-  	end
 
-  	array.to_json
+    Team.all.each do |team|
+      hash = {}
+      hash[:value] = team.name
+      hash[:team_id] = team.id
+      hash[:division] = team.division.name
+      hash[:division_id] = team.division.id
+      hash[:league] = team.league.name
+      hash[:league_id] = team.league.id
+      array.push(hash)
+    end
+
+    array.to_json
   end
 end
